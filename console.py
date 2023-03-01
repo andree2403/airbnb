0df34cd2b76ee863c772db6be79d2181abfcca6a
andree2403 <cjmorgan2589@gmail.com>
2023-03-01 14:22:56 +0000

    Tasks

--- console.py
+++ console.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """shebang"""
 import cmd
+from models import storage
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb)"
@@ -37,8 +38,65 @@ class HBNBCommand(cmd.Cmd):
         else:
             x = eval("{}()".format(line))
             storage.save()
+            print("{}".format(x.id))
 
+    def do_show(self, line):
+        linesplit = line.split(" ")
+        if not line:
+            print("** class name missing **")
+            return
+        if linesplit[0] not in self.class_names:
+            print("** class doesn't exist **")
+            return
+        if len(linesplit) == 1:
+            print("** instance id missing **")
+            return
+        try:
+            line = line.replace(' ', '.')
+            all_obj = storage.all()
+            obj = all_obj[line]
+            print(obj)
+        except Exception:
+            print("** no instance found **")
 
+    def do_destroy(self, line):
+        linesplit = line.split(" ")
+        if not line:
+            print("** class doesn't exis")
+            return
+        if linesplit[0] not in self.class_names:
+            print("** class doesn't exist **")
+            return
+        if len(linesplit) == 1:
+            print("** instance id missing **")
+            return
+        try:
+            line = line.replace(' ', '.')
+            all_obj = storage.all()
+            del all_obj[line]
+            storage.save()
+        except Exception:
+            print("** no instance found **")
+
+    def do_all(self, line):
+        if not line:
+            obct = []
+            all_obj = storage.all()
+            for items in all_obj.values():
+                obj_list.append(str(items))
+            print(obj_list)
+            return
+        if line in self.class_names:
+            obct = []
+            all_obj = storage.all()
+            for items in all_obj.values():
+                obj_list.append(str(items))
+            print(obj_list)
+            return
+        else:
+            print("** class doesn't exist **")
+
+    def do_
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()

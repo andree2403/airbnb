c97755c0c7722896839245a9881aacd3121f1688
andree2403 <cjmorgan2589@gmail.com>
2023-03-01 19:46:36 +0000

    Tasks

--- console.py
+++ console.py
@@ -2,6 +2,13 @@
 """shebang"""
 import cmd
 from models import storage
+from models.base_model import BaseModel
+from models.user import User
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb)"
@@ -41,7 +48,7 @@ class HBNBCommand(cmd.Cmd):
             print("{}".format(x.id))
 
     def do_show(self, line):
-        linesplit = line.split(" ")
+        linesplit = line.split()
         if not line:
             print("** class name missing **")
             return
@@ -60,7 +67,7 @@ class HBNBCommand(cmd.Cmd):
             print("** no instance found **")
 
     def do_destroy(self, line):
-        linesplit = line.split(" ")
+        linesplit = line.split()
         if not line:
             print("** class doesn't exis")
             return
@@ -71,9 +78,9 @@ class HBNBCommand(cmd.Cmd):
             print("** instance id missing **")
             return
         try:
-            line = line.replace(' ', '.')
+            lines = line.replace(' ', '.')
             all_obj = storage.all()
-            del all_obj[line]
+            del all_obj[lines]
             storage.save()
         except Exception:
             print("** no instance found **")
@@ -83,20 +90,46 @@ class HBNBCommand(cmd.Cmd):
             obct = []
             all_obj = storage.all()
             for items in all_obj.values():
-                obj_list.append(str(items))
-            print(obj_list)
+                obct.append(str(items))
+            print(obct)
             return
         if line in self.class_names:
             obct = []
             all_obj = storage.all()
             for items in all_obj.values():
-                obj_list.append(str(items))
-            print(obj_list)
+                obct.append(str(items))
+            print(obct)
             return
         else:
             print("** class doesn't exist **")
 
-    def do_
+    def do_update(self, line):
+        if not line:
+            print("** class name missing **")
+            return
+        linesplit = line.split()
+        if linesplit[0] not in self.class_names:
+            print("** class doesn't exist **")
+            return
+        if len(linesplit) == 1:
+            print("** instance id missing **")
+            return
+        if len(linesplit) == 2:
+            print("** attribute name missing **")
+            return
+        if len(linesplit) == 3:
+            print("** value missing **")
+            return
+        try:
+            line = line.replace(' ', '.', 1)
+            linesplit = line.split()
+            all_obj = storage.all()
+            obj = all_obj[linesplit[0]]
+            setattr(obj, linesplit[1], linesplit[2])
+            storage.save()
+        except Exception:
+            print("** no instance found **")
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
---
+++ models/state.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""shebang"""
+from models.base_model import BaseModel
+
+class State(BaseModel):
+    name = ""
